04e344a39a36b1702f8b7ac999f8fd320e9d1bf9
Wagner Rosa <wagnerrosa@users.noreply.github.com>
2026-03-18 18:11:21 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Planton UI",
   description: "Planton Design System",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/favicon.ico" },
+    ],
+    apple: "/favicon/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
