2f07de1a73652f803365bd5f0a29201ae813c6f5
Wagner Rosa <wagnerrosa@users.noreply.github.com>
2026-04-07 12:07:25 -0300

    4xl

--- src/screens/mostra/relatorio/RelatorioScreen.tsx
+++ src/screens/mostra/relatorio/RelatorioScreen.tsx
@@ -95,7 +95,7 @@ export function RelatorioScreen() {
     <>
       <MostraNavbarSync breadcrumbs={[{ label: 'Relatório Geral' }]} />
 
-      <div className="px-6 pb-6 pt-10 space-y-6 max-w-3xl">
+      <div className="px-6 pb-6 pt-10 space-y-6 max-w-4xl">
         <Heading as="h1" size="heading-lg">Relatório Geral</Heading>
 
         <Alert variant="info">
